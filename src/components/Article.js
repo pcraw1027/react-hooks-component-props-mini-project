1036b9022607ddc1b8093a780c6bbcf5bca562bb
petercrawford <pcraw1027@gmail.com>
2021-04-12 07:25:09 -0400

    Done.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 
-function Article({id, title, date, preview, minutes}) {
-    return ( <article>
+function Article({id, title, date="January 1, 1970" , preview, minutes}) {
+    return ( <article key={id}>
         <h3>{title}</h3>
         <small>{date}</small>
         <p>{preview}</p>
